e14ab50477401b97e9d7c6fadbff9b2449805961
banakid <banakid7@gmail.com>
2016-07-30 10:10:51 +0900

    test

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /Users/yasu/Program/gnutella/gnutella
+# Build directory: /Users/yasu/Program/gnutella/gnutella/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(MyTest "/Users/yasu/Program/gnutella/gnutella/build/test/Test")
+subdirs(src)
+subdirs(test)
---
+++ build/test/CMakeFiles/Test.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/yasu/Program/gnutella/gnutella/test/gtest_myint.cpp" "/Users/yasu/Program/gnutella/gnutella/build/test/CMakeFiles/Test.dir/gtest_myint.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/Users/yasu/Program/gnutella/gnutella/build/src/CMakeFiles/myint.dir/DependInfo.cmake"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../test/../extsrc/googletest/googletest/include"
+  )
+set(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
